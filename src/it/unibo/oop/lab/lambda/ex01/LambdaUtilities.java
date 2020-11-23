b8e8584505ad1d30d18ed891755ccfd868eaebe5
Sofia Tosi - sofia.tosi4@studio.unibo.it <sofia.tosi4@studio.unibo.it>
2020-11-23 12:46:02 +0100

    es 1

--- src/it/unibo/oop/lab/lambda/ex01/LambdaUtilities.java
+++ src/it/unibo/oop/lab/lambda/ex01/LambdaUtilities.java
@@ -1,7 +1,9 @@
 package it.unibo.oop.lab.lambda.ex01;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -58,10 +60,14 @@ public final class LambdaUtilities {
      *         otherwise.
      */
     public static <T> List<Optional<T>> optFilter(final List<T> list, final Predicate<T> pre) {
+        final List<Optional<T>> lista = new ArrayList<>();
+        list.forEach(ele -> lista.add(Optional.of(ele).filter(pre)));
+
+
         /*
          * Suggestion: consider Optional.filter
          */
-        return null;
+        return lista;
     }
 
     /**
@@ -77,10 +83,16 @@ public final class LambdaUtilities {
      *         based on the mapping done by the function
      */
     public static <R, T> Map<R, Set<T>> group(final List<T> list, final Function<T, R> op) {
-        /*
-         * Suggestion: consider Map.merge
-         */
-        return null;
+        Map<R, Set<T>> mappa = new HashMap<>();
+        list.forEach((e) -> 
+        mappa.merge(op.apply(e), new HashSet<T>(Arrays.asList(e)),
+                (set1, set2) -> {
+                    set1.addAll(set2); return set1; })
+        );
+            /*
+             * Suggestion: consider Map.merge
+             */
+            return mappa;
     }
 
     /**
@@ -96,12 +108,16 @@ public final class LambdaUtilities {
      *         by the supplier
      */
     public static <K, V> Map<K, V> fill(final Map<K, Optional<V>> map, final Supplier<V> def) {
+        Map<K, V> mappa = new HashMap();
+        map.forEach((k, v) -> {
+            mappa.put(k, v.orElse(def.get())); });
+
         /*
          * Suggestion: consider Optional.orElse
          * 
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return null;
+        return mappa;
     }
 
     /**
